a0f0b999e9d52b7345c00578dc7a10b9a073f8df
BrianLevin <bml201095@gmail.com>
2021-04-17 12:02:10 -0400

    converted the data we got back from hexidecimal code to a javascript object

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&units=imper
     https.get(url, function(responce){
 
         console.log(responce.statusCode);
+
+        responce.on("data", function(data){
+
+            const weatherData= JSON.parse(data);
+            console.log(weatherData);
+
+        });
     });
     res.send("Server is up and running.")
 
