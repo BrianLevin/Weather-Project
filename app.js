b7423c5ec009ab3b01b7b406c86a2b90efe758f6
BrianLevin <bml201095@gmail.com>
2021-04-17 14:05:46 -0400

    used the post request to parse through the data for the city

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require("express");
-
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true})) 
 
-const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 
@@ -18,38 +17,39 @@ app.get("/", function (req, res) {
 
 app.post("/", function(req,res){
 
-    console.log("Post request recieved")
+const query = req.body.cityName;
+
+apiKey = "bcd5953a34ae60d3a19dd457376dfad5";
+const unit= "imperial";
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units="  + unit + "&appid=" + apiKey;
+https.get(url, function (responce) {
+console.log(responce.statusCode);
+
+responce.on("data", function (data) {
+  const weatherData = JSON.parse(data);
+
+  const temp = weatherData.main.temp;
+
+  const weatherDescription = weatherData.weather[0].description;
+
+  const icon = weatherData.weather[0].icon;
+
+  imageURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png"
+
+  res.write("<p>The weather is currently " + weatherDescription + "</p>");
+  res.write(
+    "<h1>The temperature in " + query +  " is " + temp + " degrees Fahrenheit.</h1>"
+  );
+
+  res.write("<img src =" +imageURL + ">" );
+  res.send();
+});
+});
+   
 })
 
-// const query = "London";
 
-// apiKey = "bcd5953a34ae60d3a19dd457376dfad5";
-// const unit= "metric";
-
-// const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units="  + unit + "&appid=" + apiKey;
-// https.get(url, function (responce) {
-// console.log(responce.statusCode);
-
-// responce.on("data", function (data) {
-//   const weatherData = JSON.parse(data);
-
-//   const temp = weatherData.main.temp;
-
-//   const weatherDescription = weatherData.weather[0].description;
-
-//   const icon = weatherData.weather[0].icon;
-
-//   imageURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png"
-
-//   res.write("<p>The weather is currently " + weatherDescription + "</p>");
-//   res.write(
-//     "<h1>The temperature in Paris is " + temp + " degrees Fahrenheit.</h1>"
-//   );
-
-//   res.write("<img src =" +imageURL + ">" );
-//   res.send();
-// });
-// });
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000!");
