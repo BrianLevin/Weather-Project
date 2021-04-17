bc04cc2d851807099f41773f4cdb07156dbde663
BrianLevin <bml201095@gmail.com>
2021-04-17 11:49:43 -0400

    got the https request to work to fetch the weather api data

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app =  express();
 
 app.get("/", function(req,res){
 
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&units=imperial&appid=bcd5953a34ae60d3a19dd457376dfad5";
+    https.get(url, function(responce){
+
+        console.log(responce);
+    });
     res.send("Server is up and running.")
 
 });
