bc5858fd552c7a78254c34e2ae96aeee16d37b08
BrianLevin <bml201095@gmail.com>
2021-04-17 13:19:23 -0400

    created variables to hold the api key, unit, and city query

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const https = require("https");
 const app = express();
 
 app.get("/", function (req, res) {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Paris&units=imperial&appid=bcd5953a34ae60d3a19dd457376dfad5";
+
+    const query = "London";
+
+    apiKey = "bcd5953a34ae60d3a19dd457376dfad5";
+ const unit= "metric";
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units="  + unit + "&appid=" + apiKey;
   https.get(url, function (responce) {
     console.log(responce.statusCode);
 
