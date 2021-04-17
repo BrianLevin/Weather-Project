47d785cb2b8f56c7693d022c5e7501476ddfd56f
BrianLevin <bml201095@gmail.com>
2021-04-17 11:54:26 -0400

    Added a responce code and see i got 200

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get("/", function(req,res){
 const url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&units=imperial&appid=bcd5953a34ae60d3a19dd457376dfad5";
     https.get(url, function(responce){
 
-        console.log(responce);
+        console.log(responce.statusCode);
     });
     res.send("Server is up and running.")
 
